c7e72e2a9f6772c04ff6fd7db923608a11018c9c
pedrovp161 <pedrovp161@gmail.com>
2024-02-22 15:24:09 -0300

    total_sum

--- Ovary_v4/processamento.py
+++ Ovary_v4/processamento.py
@@ -26,9 +26,22 @@ print(adatas_dir)
 
 for i in adatas_dir:
     adata = adatas_dir[i]
-    print(adata)
+    # print(adata)
     adata.var["mt"] = adata.var["gene_ids"].str.startswith("MT-")
     sc.pp.calculate_qc_metrics(adata, qc_vars=["mt"], inplace=True)
     adata.obs['pct_counts_mt'] = np.sum(adata[:, adata.var_names.str.startswith('MT-')].X, axis=1) / np.sum(adata.X, axis=1) * 100# pct_counts_mt é a porcentagem de reads mitocondriais e é equivalente 
     adata.obs['out_count'] = is_outlier(adata.obs['total_counts']).astype(str)# total_counts é o numero de reads e é o equivalente a n_counts
     adata.obs['out_feature'] = is_outlier(adata.obs['n_genes_by_counts']).astype(str)# n_genes_by_counts = n_genes
+
+    # Filtra os dados com base na porcentagem de genes mitocondriais, contagens de RNA e características de RNA.
+    adata = adata[adata.obs["pct_counts_mt"] < 20]
+    
+# Contagem do número de células antes e depois da filtragem
+total_sum_after = []
+for i in adatas_dir:
+    adata = adatas_dir[i]
+    cells_before_by_sample = sum([adata.n_obs for adata in adatas_dir[i]])
+    total_sum_after.append(cells_before_by_sample)
+print(sum(total_sum_after)) # falta o antes
+
+# Define o diretório para salvar os objetos Anndata filtrados
